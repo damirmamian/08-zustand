c8538e36ee175265288c08df3f99150e03a3aa33
damirmamian <damirmamian@gmail.com>
2026-05-16 16:46:21 +0300

    solved

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,9 +1,33 @@
 import { fetchNoteById } from "@/lib/api";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import NoteDetailsClient from "./NoteDetails.client";
-
+import { Metadata } from "next";
 type Props = {
     params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { id } = await params;
+    const note = await fetchNoteById(id);
+
+    return {
+        title: note.title,
+        description: note.content.slice(0, 160),
+        openGraph: {
+            title: note.title,
+            description: note.content.slice(0, 160),
+            url: `https://08-zustand-ruddy-mu.vercel.app/notes/${id}`,
+            siteName: "NoteHub",
+            images: [
+                {
+                    url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+                    width: 1200,
+                    height: 630,
+                    alt: "NoteHub Open Graph Image",
+                }
+            ],
+        },
+    };
 };
 
 const NoteDetails = async ({ params }: Props) => {
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -5,10 +5,36 @@ import {
 } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
+import { Metadata } from "next";
 
 type Props = {
     params: Promise<{ slug: string[] }>;
 }
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { slug } = await params;
+    const titleTag = slug[0] === "all" ? "All Notes" : `Notes tagged with "${slug[0]}"`;
+    const descriptionTag = slug[0] === "all" ? "Browse all notes in NoteHub." : `Explore notes tagged with "${slug[0]}" in NoteHub.`;
+
+    return {
+        title: titleTag,
+        description: descriptionTag,
+        openGraph: {
+            title: titleTag,
+            description: descriptionTag,
+            url: `https://08-zustand-ruddy-mu.vercel.app/notes/filter/${slug[0]}`,
+            siteName: "NoteHub",
+            images: [
+                {
+                    url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+                    width: 1200,
+                    height: 630,
+                    alt: "NoteHub Open Graph Image",
+                }
+            ],
+        },
+    };
+};
 
 const Notes = async ({ params }: Props) => {
     const { slug } = await params;
