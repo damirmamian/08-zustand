099702f58ae603f52d6d516744293e9a3fc56330
damirmamian <damirmamian@gmail.com>
2026-05-16 17:12:49 +0300

    task 6

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -1,74 +1,86 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import { useDebounce } from 'use-debounce';
+
 import Loader from '@/components/Loader/Loader';
 import ServerError from '@/components/ServerError/ServerError';
 import NoNotesError from '@/components/NoNotesError/NoNotesError';
-import { useDebouncedCallback } from 'use-debounce';
 import SearchBox from '@/components/SearchBox/SearchBox';
-import css from './Notes.module.css';
-import { useState } from 'react';
-import { keepPreviousData, useQuery } from '@tanstack/react-query';
-import { fetchNotes } from '@/lib/api';
 import Pagination from '@/components/Pagination/Pagination';
 import NoteList from '@/components/NoteList/NoteList';
-/* import Modal from '@/components//Modal/Modal';
-import NoteForm from '@/components/NoteForm/NoteForm'; */
+
+import { fetchNotes } from '@/lib/api';
+
 import { Toaster } from 'react-hot-toast';
 import Link from 'next/link';
 
+import css from './Notes.module.css';
 
 type NoteClientProps = {
-    tag: string | undefined,
+    tag: string | undefined;
 };
 
 export default function NotesClient({ tag }: NoteClientProps) {
-
-    const [nameNote, setNameNote] = useState('');
+    const [inputValue, setInputValue] = useState('');
     const [page, setPage] = useState(1);
 
+    const [debouncedValue] = useDebounce(inputValue, 300);
+
+
+
     const { data, isSuccess, isLoading, isError } = useQuery({
-        queryKey: ['notes', nameNote, page, tag],
-        queryFn: () => fetchNotes(nameNote, page, tag),
+        queryKey: ['notes', debouncedValue, page, tag],
+        queryFn: () => fetchNotes(debouncedValue, page, tag),
         placeholderData: keepPreviousData,
-        refetchOnMount: false,
-    })
+    });
 
     const totalPages = data?.totalPages || 0;
 
-    const handleSearchChange = useDebouncedCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-        setNameNote(event.target.value);
+    const handleSearchChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setInputValue(event.target.value);
         setPage(1);
-    }, 300);
+    };
 
-    const onPageChange = (page: number) => {
-        setPage(page);
-    }
-
-    /* const [modalOpen, setModalOpen] = useState(false);
-	
-    const openModal = () => setModalOpen(true);
-    const closeModal = () => setModalOpen(false); */
+    const onPageChange = (selectedPage: number) => {
+        setPage(selectedPage);
+    };
 
     return (
         <div className={css.app}>
             <div className={css.toolbar}>
-                <SearchBox onChange={handleSearchChange} />
+                <SearchBox
+                    value={inputValue}
+                    onChange={handleSearchChange}
+                />
+
                 {data && data.totalPages > 1 && (
-                    <Pagination totalPages={totalPages} forcePage={page} onPageChange={onPageChange} />
+                    <Pagination
+                        totalPages={totalPages}
+                        forcePage={page}
+                        onPageChange={onPageChange}
+                    />
                 )}
-                <Link className={css.button} href="/notes/action/create">Create note +</Link>
-                {/* <button className={css.button} onClick={openModal}>Create note +</button> */}
+
+                <Link
+                    className={css.button}
+                    href="/notes/action/create"
+                >
+                    Create note +
+                </Link>
             </div>
+
             {isLoading && <Loader />}
             {isError && <ServerError />}
             {data && data.notes.length === 0 && <NoNotesError />}
-            {isSuccess && data && (<NoteList notes={data.notes} />)}
+            {isSuccess && data && (
+                <NoteList notes={data.notes} />
+            )}
 
-            {/* {modalOpen && (
-				<Modal onClose={closeModal}>
-					<NoteForm onClose={closeModal} />
-				</Modal>)} */}
             <Toaster />
         </div>
     );
-};
+}
--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -1,16 +1,21 @@
 import css from './SearchBox.module.css';
 
 interface SearchBoxProps {
+    value: string;
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function SearchBox({ onChange }: SearchBoxProps) {
+export default function SearchBox({
+    value,
+    onChange,
+}: SearchBoxProps) {
     return (
         <input
             className={css.input}
             type="text"
             placeholder="Search notes"
+            value={value}
             onChange={onChange}
         />
-    )
+    );
 }
